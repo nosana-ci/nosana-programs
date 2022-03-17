0e144d55af20dfd19081279a91f2eaf2368196da
Sjoerd Dijkstra <sjoerd_d60@hotmail.com>
2022-03-17 11:41:59 +0100

    Update token and build script

--- programs/nosana-jobs/src/ids.rs
+++ programs/nosana-jobs/src/ids.rs
@@ -7,7 +7,7 @@ declare_id!("nosJwntQe4eEnFC2mCsY9J15qw7kRQCDqcjcPj6aPbR");
 pub mod nos {
     use anchor_lang::declare_id;
     #[cfg(feature = "mainnet")]
-    declare_id!("nosXBVoaCTtYdLvKY6Csb4AC8JCdQKKAaWYtx2ZMoo7");
+    declare_id!("TSTntXiYheDFtAdQ1pNBM2QQncA22PCFLLRr53uBa8i");
     #[cfg(not(feature = "mainnet"))]
     declare_id!("testsKbCqE8T1ndjY4kNmirvyxjajKvyp1QTDmdGwrp");
 }
---
+++ scripts/build.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# exit upon failure
+set -e
+
+# setup
+[[ -n "${DEBUG_SCRIPT}" ]] && set -xv
+
+# colors
+RED="\033[1;91m"
+CYAN="\033[1;36m"
+GREEN="\033[1;32m"
+WHITE="\033[1;38;5;231m"
+RESET="\n\033[0m"
+
+# functions
+log_std() { echo -e "${CYAN}==> ${WHITE}${1}${RESET}"; }
+log_err() { echo -e "${RED}==> ${WHITE}${1}${RESET}"; }
+
+# variables
+tag=$(git tag  | grep -E '^v[0-9]' | sort -V | tail -1)
+toml=programs/nosana_jobs/Cargo.toml
+
+# prepare cargo for mainnet
+log_std "Setup for mainnet"
+sed -i "s/#default/default/" "${toml}"
+
+log_std "Set version to ${GREEN}${tag}"
+sed -i "s/0.1.0/${tag:1}/" "${toml}"
+
+# regular build
+anchor build
