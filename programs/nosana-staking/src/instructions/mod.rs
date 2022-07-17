2a565a927dc8d3f0240b2d199e198dd07095a5ce
Jesse Eisses <jesse.eisses@gmail.com>
2022-07-17 17:26:00 +0200

    Fix rustfmt formatting

--- programs/nosana-staking/src/instructions/mod.rs
+++ programs/nosana-staking/src/instructions/mod.rs
@@ -2,18 +2,18 @@
 
 pub mod claim;
 pub mod emit_rank;
+pub mod extend;
 pub mod init_vault;
 pub mod restake;
 pub mod stake;
 pub mod topup;
-pub mod extend;
 pub mod unstake;
 
 pub use claim::*;
 pub use emit_rank::*;
+pub use extend::*;
 pub use init_vault::*;
 pub use restake::*;
 pub use stake::*;
 pub use topup::*;
-pub use extend::*;
 pub use unstake::*;
