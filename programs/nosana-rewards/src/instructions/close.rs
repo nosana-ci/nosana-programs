d0230a4e508de86a100835790f6b3734cd0fae52
Jesse Eisses <jesse.eisses@gmail.com>
2022-07-29 13:26:33 +0200

    Add UncheckedAccount for reward close instruction

--- programs/nosana-rewards/src/instructions/close.rs
+++ programs/nosana-rewards/src/instructions/close.rs
@@ -7,11 +7,12 @@ use nosana_staking::program::NosanaStaking;
 pub struct Close<'info> {
     #[account(mut, seeds = [ b"stats" ], bump = stats.bump)]
     pub stats: Account<'info, StatsAccount>,
-    #[account(mut, close = authority, seeds = [ b"reward", staker.key().as_ref()], bump = reward.bump)]
+    #[account(mut, close = authority, constraint = staker.key() == reward.authority, bump = reward.bump)]
     pub reward: Box<Account<'info, RewardAccount>>,
     #[account(owner = staking_program.key(), constraint = staker.key() == stake.authority)]
     pub stake: Account<'info, StakeAccount>,
-    pub staker: AccountInfo<'info>,
+    /// CHECK: this is the owner of the stake and reward, and is optionally the signer
+    pub staker: UncheckedAccount<'info>,
     #[account(mut)]
     pub authority: Signer<'info>,
     pub staking_program: Program<'info, NosanaStaking>,
