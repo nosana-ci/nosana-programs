a21790b7ea211ca219849d862148e0ce79a58e27
Laurens Verspeek <l.verspeek@gmail.com>
2022-08-04 16:58:36 +0200

    add sync function. TODO: needs to be tested

--- programs/nosana-rewards/src/instructions/sync.rs
+++ programs/nosana-rewards/src/instructions/sync.rs
@@ -4,45 +4,42 @@ use nosana_common::{nos, staking, transfer_tokens, NosanaError};
 use nosana_staking::StakeAccount;
 
 #[derive(Accounts)]
-pub struct Claim<'info> {
+pub struct Sync<'info> {
     #[account(mut, seeds = [ b"stats" ], bump = stats.bump)]
     pub stats: Account<'info, StatsAccount>,
     #[account(mut, seeds = [ nos::ID.key().as_ref() ], bump)]
     pub ata_vault: Box<Account<'info, TokenAccount>>,
     #[account(mut)]
     pub ata_to: Box<Account<'info, TokenAccount>>,
-    #[account(owner = staking::ID, has_one = authority)]
+    #[account(owner = staking::ID, has_one=authority)]
     pub stake: Account<'info, StakeAccount>,
     #[account(mut, seeds = [ b"reward", authority.key().as_ref() ], bump = reward.bump)]
     pub reward: Box<Account<'info, RewardAccount>>,
     #[account(mut)]
-    pub authority: Signer<'info>,
+    pub authority: Account<'info>,
     pub token_program: Program<'info, Token>,
 }
 
-pub fn handler(ctx: Context<Claim>) -> Result<()> {
-    // get and check stake, reward, and stats account
+pub fn handler(ctx: Context<Sync>) -> Result<()> {
+    // get and check stake + reward account
     let stake: &Account<StakeAccount> = &ctx.accounts.stake;
     let reward: &mut Box<Account<RewardAccount>> = &mut ctx.accounts.reward;
-    let stats: &mut Account<StatsAccount> = &mut ctx.accounts.stats;
     require!(stake.time_unstake == 0, NosanaError::StakeAlreadyUnstaked);
-    require!(stake.xnos >= reward.xnos, NosanaError::StakeDecreased);
 
-    // pay-out tokens
-    transfer_tokens(
-        ctx.accounts.token_program.to_account_info(),
-        ctx.accounts.ata_vault.to_account_info(),
-        ctx.accounts.ata_to.to_account_info(),
-        ctx.accounts.ata_vault.to_account_info(),
-        *ctx.bumps.get("ata_vault").unwrap(),
-        reward.get_amount(stats.rate),
-    )?;
+    // determine pay-out
+    let stats: &mut Account<StatsAccount> = &mut ctx.accounts.stats;
+    let amount: u128 = reward
+        .reflection
+        .checked_div(stats.rate)
+        .unwrap()
+        .checked_sub(reward.xnos)
+        .unwrap();
 
     // decrease the reflection pool
     stats.remove_rewards_account(reward.reflection, reward.xnos);
 
     // re-enter the pool with the current
-    reward.update(stats.add_rewards_account(stake.xnos, 0), stake.xnos);
+    reward.update(stats.add_rewards_account(stake.xnos, amount), stake.xnos);
 
     // finish
     Ok(())
