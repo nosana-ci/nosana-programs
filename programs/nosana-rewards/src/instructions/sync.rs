df9138b28111623ba6330f60de98f9bc76b9a71b
Laurens Verspeek <l.verspeek@gmail.com>
2022-08-05 11:02:50 +0200

    some fixes for sync

--- programs/nosana-rewards/src/instructions/sync.rs
+++ programs/nosana-rewards/src/instructions/sync.rs
@@ -1,18 +1,15 @@
 use crate::*;
-use anchor_spl::token::{Token, TokenAccount};
-use nosana_common::{nos, staking, transfer_tokens, NosanaError};
+use nosana_common::{nos, staking, reward, NosanaError};
 use nosana_staking::StakeAccount;
 
 #[derive(Accounts)]
 pub struct Sync<'info> {
     #[account(mut, seeds = [ b"stats" ], bump = stats.bump)]
     pub stats: Account<'info, StatsAccount>,
-    #[account(owner = staking::ID, has_one=authority)]
+    #[account(owner = staking::ID)]
     pub stake: Account<'info, StakeAccount>,
-    #[account(mut, seeds = [ b"reward", authority.key().as_ref() ], bump = reward.bump)]
-    pub reward: Box<Account<'info, RewardAccount>>,
-    #[account(mut)]
-    pub authority: Account<'info>
+    #[account(mut, owner = reward::ID)]
+    pub reward: Box<Account<'info, RewardAccount>>
 }
 
 pub fn handler(ctx: Context<Sync>) -> Result<()> {
@@ -20,8 +17,11 @@ pub fn handler(ctx: Context<Sync>) -> Result<()> {
     let stake: &Account<StakeAccount> = &ctx.accounts.stake;
     let reward: &mut Box<Account<RewardAccount>> = &mut ctx.accounts.reward;
     require!(stake.time_unstake == 0, NosanaError::StakeAlreadyUnstaked);
-
-    // determine pay-out
+    require!(
+        stake.authority == reward.authority,
+        NosanaError::Unauthorized
+    );
+    // determine current pending pay-out
     let stats: &mut Account<StatsAccount> = &mut ctx.accounts.stats;
     let amount: u128 = reward
         .reflection
