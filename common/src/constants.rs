c5b2e6512349957571f33709f7982d96c582cbcc
Jesse Eisses <jesse.eisses@gmail.com>
2022-08-22 17:25:30 +0200

    Set initial reflection rate for reward pool

--- common/src/constants.rs
+++ common/src/constants.rs
@@ -1,3 +1,5 @@
 // number of decimals for the Nosana native token
 // https://explorer.solana.com/address/nosXBVoaCTtYdLvKY6Csb4AC8JCdQKKAaWYtx2ZMoo7
 pub const NOS_DECIMALS: u64 = 1_000_000;
+
+pub const NOS_TOTAL_SUPPLY: u128 = 1_000_000 * 100_000_000;
