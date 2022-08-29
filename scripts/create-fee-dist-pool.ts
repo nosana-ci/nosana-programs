a97c17103e526faa2fcd357114cff05a71b2df7c
Sjoerd Dijkstra <sjoerd_d60@hotmail.com>
2022-08-29 16:41:02 +0200

    Add script to initialize the mining pool for stakers

---
+++ scripts/create-fee-dist-pool.ts
@@ -0,0 +1,51 @@
+import { AnchorProvider, Program, setProvider, web3, BN } from '@project-serum/anchor';
+import { utf8 } from '@project-serum/anchor/dist/cjs/utils/bytes';
+import { Keypair, PublicKey } from '@solana/web3.js';
+import { NosanaPools } from '../target/types/nosana_pools';
+import { getAssociatedTokenAddress, TOKEN_PROGRAM_ID } from '@solana/spl-token';
+
+async function main() {
+  // anchor
+  const provider = AnchorProvider.env();
+  setProvider(provider);
+
+  const emission = 253678; // lamport-NOS per second
+  const start_time = 1661780725; // 08/29/2022 @ 1:45pm
+  const closeable = true;
+
+  // public keys
+  const stakingId = new PublicKey('nosPdZrfDzND1LAR28FLMDEATUPK53K8xbRBXAirevD');
+  const rewardsId = new PublicKey('nosRB8DUV67oLNrL45bo2pFLrmsWPiewe2Lk2DRNYCp');
+  const mint = new PublicKey('testsKbCqE8T1ndjY4kNmirvyxjajKvyp1QTDmdGwrp');
+
+  // the vanity address for this pool
+  const keyPair = Keypair.fromSecretKey(new Uint8Array(require('../poF4cdcnisqUSBCbfvf4T9Hmvz1sTdhfhGzfPqoQZks.json')));
+
+  // program
+  const idl = await Program.fetchIdl(stakingId.toString());
+  const program = new Program(idl, stakingId) as Program<NosanaPools>;
+
+  // PDAs
+  const accounts = {
+    authority: provider.wallet.publicKey,
+    beneficiary: (await PublicKey.findProgramAddress([mint.toBuffer()], rewardsId))[0],
+    mint: mint,
+    pool: keyPair.publicKey,
+    rent: web3.SYSVAR_RENT_PUBKEY,
+    systemProgram: web3.SystemProgram.programId,
+    tokenProgram: TOKEN_PROGRAM_ID,
+    user: await getAssociatedTokenAddress(mint, provider.wallet.publicKey),
+    vault: (await PublicKey.findProgramAddress([utf8.encode('vault'), keyPair.publicKey.toBuffer()], stakingId))[0],
+  };
+
+  // open pool
+  let tx = await program.methods
+    .open(new BN(emission), new BN(start_time), closeable)
+    .accounts(accounts)
+    .signers([keyPair])
+    .rpc();
+  console.log(tx);
+}
+
+console.log('Running client.');
+main().then(() => console.log('Success'));
