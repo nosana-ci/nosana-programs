e7d8cff0009615d7e95686e91d4e63e2234fa50f
Sjoerd Dijkstra <sjoerd_d60@hotmail.com>
2022-08-29 17:13:23 +0200

    Move mint to pool config

--- scripts/create-fee-dist-pool.ts
+++ scripts/create-fee-dist-pool.ts
@@ -14,9 +14,9 @@ async function main() {
   const keyPair = Keypair.fromSecretKey(new Uint8Array(require(poolConfig.poolKey)));
 
   // public keys
+  const mint = new PublicKey(poolConfig.mint);
   const poolsId = new PublicKey('nosPdZrfDzND1LAR28FLMDEATUPK53K8xbRBXAirevD');
   const rewardsId = new PublicKey('nosRB8DUV67oLNrL45bo2pFLrmsWPiewe2Lk2DRNYCp');
-  const mint = new PublicKey('devr1BGQndEW5k5zfvG5FsLyZv1Ap73vNgAHcQ9sUVP');
 
   // program
   const idl = await Program.fetchIdl(poolsId.toString());
@@ -26,7 +26,7 @@ async function main() {
   const accounts = {
     authority: provider.wallet.publicKey,
     beneficiary: (await PublicKey.findProgramAddress([mint.toBuffer()], rewardsId))[0],
-    mint: mint,
+    mint,
     pool: keyPair.publicKey,
     rent: web3.SYSVAR_RENT_PUBKEY,
     systemProgram: web3.SystemProgram.programId,
