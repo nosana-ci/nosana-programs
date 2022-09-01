69f659e955ca033fb120f8afc12be9cd4d785725
Sjoerd Dijkstra <sjoerd_d60@hotmail.com>
2022-09-01 11:37:58 +0200

    Correct type ignore

--- scripts/create-fee-dist-pool.ts
+++ scripts/create-fee-dist-pool.ts
@@ -1,11 +1,11 @@
 import { AnchorProvider, Program, setProvider, web3, BN, Idl } from '@project-serum/anchor';
 import { utf8 } from '@project-serum/anchor/dist/cjs/utils/bytes';
 import { Keypair, PublicKey } from '@solana/web3.js';
-import { NosanaPools } from '../target/types/nosana_pools';
+import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
 import { pda } from '../tests/utils';
 import poolConfig = require('../tests/data/pool.json');
 // @ts-ignore
-import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
+import { NosanaPools } from '../target/types/nosana_pools';
 
 async function main() {
   // anchor
