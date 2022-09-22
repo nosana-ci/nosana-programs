fba02c413e84d0d842dbde70ae75d60820b7f135
Sjoerd Dijkstra <sjoerd_d60@hotmail.com>
2022-09-22 15:31:13 +0200

    Deploy the first node queue on devnet

---
+++ scripts/create-nodes-queue.ts
@@ -0,0 +1,54 @@
+import { AnchorProvider, Program, setProvider, Wallet, web3, BN, Idl } from '@project-serum/anchor';
+import { Keypair, PublicKey } from '@solana/web3.js';
+import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
+// @ts-ignore
+import { NosanaJobs } from '../target/types/nosana_jobs';
+import { constants } from '../tests/contstants';
+import { pda } from '../tests/utils';
+
+async function main() {
+  // anchor
+  const provider = AnchorProvider.env();
+  setProvider(provider);
+  const wallet = provider.wallet as Wallet;
+
+  // throw away keypair
+  const nodesKey = Keypair.generate();
+
+  // public keys
+  const mint = new PublicKey('devr1BGQndEW5k5zfvG5FsLyZv1Ap73vNgAHcQ9sUVP');
+  const nodes = nodesKey.publicKey;
+  const programId = new PublicKey('nosJhNRqr2bc9g1nfGDcXXTXvYUmxD4cVwy2pMWhrYM');
+
+  // program
+  const idl = (await Program.fetchIdl(programId.toString())) as Idl;
+  const program = new Program(idl, programId) as unknown as Program<NosanaJobs>;
+
+  // open pool
+  const tx = await program.methods
+    .init(
+      new BN(constants.decimals), // job price = 1 NOS
+      new BN(60 * 60), // job timeout = 1 hour
+      constants.jobType.default, // job type = default
+      new BN(10_000 * constants.decimals) // minimum stake = 10,000.- NOS
+    )
+    .accounts({
+      mint,
+      nodes,
+      vault: await pda([nodes.toBuffer(), mint.toBuffer()], programId),
+      authority: wallet.publicKey,
+      accessKey: new PublicKey('nftNgYSG5pbwL7kHeJ5NeDrX8c4KrG1CzWhEXT8RMJ3'),
+      rent: web3.SYSVAR_RENT_PUBKEY,
+      systemProgram: web3.SystemProgram.programId,
+      tokenProgram: TOKEN_PROGRAM_ID,
+    })
+    .signers([nodesKey])
+    .rpc();
+
+  // log data
+  console.log(`https://explorer.solana.com/address/${nodes}?cluster=devnet`);
+  console.log(`https://explorer.solana.com/tx/${tx}?cluster=devnet`);
+}
+
+console.log('Running client.');
+main().then(() => console.log('Success'));
