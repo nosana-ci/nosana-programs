36ae724e9a81aa75e84951b9cb4f9253bcc2167e
Sjoerd Dijkstra <sjoerd_d60@hotmail.com>
2022-09-26 15:45:41 +0200

    Add get-staking-address script

---
+++ scripts/get-staking-address.ts
@@ -0,0 +1,17 @@
+
+import { utf8 } from '@project-serum/anchor/dist/cjs/utils/bytes';
+import { PublicKey } from '@solana/web3.js';
+import { pda } from '../tests/utils';
+// @ts-ignore
+import { NosanaRewards } from '../target/types/nosana_rewards';
+
+async function main() {
+  const programId = new PublicKey('nosScmHY2uR24Zh751PmGj9ww9QRNHewh9H59AfrTJE');
+  const mint = new PublicKey('nosXBVoaCTtYdLvKY6Csb4AC8JCdQKKAaWYtx2ZMoo7');
+  const authority = new PublicKey(process.argv.slice(2)[0]);
+  const address = await pda([utf8.encode('stake'), mint.toBuffer(), authority.toBuffer()], programId)
+  console.log(`https://explorer.solana.com/address/${address}`);
+}
+
+console.log('Running client.');
+main().then(() => console.log('Success'));
