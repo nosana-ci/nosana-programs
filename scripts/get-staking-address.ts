f1d0dcad2ecc6daeeb3c2622cfcf3a02af7c4070
Sjoerd Dijkstra <sjoerd_d60@hotmail.com>
2022-09-26 15:46:53 +0200

    Run lint

--- scripts/get-staking-address.ts
+++ scripts/get-staking-address.ts
@@ -1,4 +1,3 @@
-
 import { utf8 } from '@project-serum/anchor/dist/cjs/utils/bytes';
 import { PublicKey } from '@solana/web3.js';
 import { pda } from '../tests/utils';
@@ -7,7 +6,7 @@ async function main() {
   const programId = new PublicKey('nosScmHY2uR24Zh751PmGj9ww9QRNHewh9H59AfrTJE');
   const mint = new PublicKey('nosXBVoaCTtYdLvKY6Csb4AC8JCdQKKAaWYtx2ZMoo7');
   const authority = new PublicKey(process.argv.slice(2)[0]);
-  const address = await pda([utf8.encode('stake'), mint.toBuffer(), authority.toBuffer()], programId)
+  const address = await pda([utf8.encode('stake'), mint.toBuffer(), authority.toBuffer()], programId);
   console.log(`https://explorer.solana.com/address/${address}`);
 }
 
