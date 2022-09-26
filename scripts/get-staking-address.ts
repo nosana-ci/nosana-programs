4607d0a902444932886dead61b45c3498ffcf247
Sjoerd Dijkstra <sjoerd_d60@hotmail.com>
2022-09-26 15:53:06 +0200

    Simplify script

--- scripts/get-staking-address.ts
+++ scripts/get-staking-address.ts
@@ -3,11 +3,15 @@ import { PublicKey } from '@solana/web3.js';
 import { pda } from '../tests/utils';
 
 async function main() {
-  const programId = new PublicKey('nosScmHY2uR24Zh751PmGj9ww9QRNHewh9H59AfrTJE');
-  const mint = new PublicKey('nosXBVoaCTtYdLvKY6Csb4AC8JCdQKKAaWYtx2ZMoo7');
-  const authority = new PublicKey(process.argv.slice(2)[0]);
-  const address = await pda([utf8.encode('stake'), mint.toBuffer(), authority.toBuffer()], programId);
-  console.log(`https://explorer.solana.com/address/${address}`);
+  const pk = await pda(
+    [
+      utf8.encode('stake'),
+      new PublicKey('nosXBVoaCTtYdLvKY6Csb4AC8JCdQKKAaWYtx2ZMoo7').toBuffer(),
+      new PublicKey(process.argv.slice(2)[0]).toBuffer(),
+    ],
+    new PublicKey('nosScmHY2uR24Zh751PmGj9ww9QRNHewh9H59AfrTJE')
+  );
+  console.log(`https://explorer.solana.com/address/${pk}`);
 }
 
 console.log('Running client.');
