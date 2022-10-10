b4b1fc351a644e592e201550a74ac2006c1b74b7
Sjoerd Dijkstra <sjoerd_d60@hotmail.com>
2022-10-10 12:16:20 +0200

    fix idl

--- programs/nosana-jobs/src/lib.rs
+++ programs/nosana-jobs/src/lib.rs
@@ -18,10 +18,8 @@ pub mod nosana_jobs {
     */
 
     /// ### Open
-    ///
     /// The `open()` instruction initializes a [MarketAccount](#market-account) and an
     /// associated [VaultAccount](#vault-account) for token deposits.
-    ///
     pub fn open(
         ctx: Context<Open>,
         job_expiration: i64,
@@ -41,9 +39,7 @@ pub mod nosana_jobs {
     }
 
     /// ### Update
-    ///
     /// The `update()` instruction updates a [MarketAccount](#market-account) configurations.
-    ///
     pub fn update(
         ctx: Context<Update>,
         job_expiration: i64,
@@ -63,11 +59,8 @@ pub mod nosana_jobs {
     }
 
     /// ### Close
-    ///
     /// The `close()` instruction closes a [MarketAccount](#market-account) and the
-    /// associated [VaultAccount](#vault-account).
-    /// The vault has to be empty of tokens.
-    ///
+    /// associated [VaultAccount](#vault-account). The vault has to be empty of tokens.
     pub fn close(ctx: Context<Close>) -> Result<()> {
         close::handler(ctx)
     }
@@ -77,19 +70,15 @@ pub mod nosana_jobs {
     */
 
     /// ### List
-    ///
     /// The `list()` instruction lists a job, with its required data.
     /// When there is a node available, a [JobAccount](#job-account) will automatically be created.
-    ///
     pub fn list(ctx: Context<List>, ipfs_job: [u8; 32]) -> Result<()> {
         list::handler(ctx, ipfs_job)
     }
 
     /// ### Recover
-    ///
     /// The `recover()` instruction recovers funds from a jobs that has been [Quit](#quit)'ed.
     /// The [JobAccount](#job-account) will automatically be created.
-    ///
     pub fn recover(ctx: Context<Recover>) -> Result<()> {
         recover::handler(ctx)
     }
@@ -99,50 +88,33 @@ pub mod nosana_jobs {
     */
 
     /// ### Work
-    ///
     /// With the `work()` instruction a node enters the [MarketAccount](#market-account) queue.
     /// When there is a job available, a [JobAccount](#job-account) will automatically be created.
-    ///
-    /// A few requirements are enforced:
-    ///
-    /// - A node needs to have a minimum stake in Nosana Staking.
-    /// - A node needs to hold an official Nosana NFT.
-    /// - A node can only enter the queue once
-    ///
     pub fn work(ctx: Context<Work>) -> Result<()> {
         work::handler(ctx)
     }
 
     /// ### Stop
-    ///
-    /// With the `stop()` instruction a node exits the node queue
-    /// from a [MarketAccount](#market-account).
-    ///
+    /// With the `stop()` instruction a node exits the node queue from a [MarketAccount](#market-account).
     pub fn stop(ctx: Context<Stop>) -> Result<()> {
         stop::handler(ctx)
     }
 
     /// ### Claim
-    ///
     /// With the `claim()` instruction a node claims a job that is [stopped](#stop).
-    ///
     pub fn claim(ctx: Context<Claim>) -> Result<()> {
         claim::handler(ctx)
     }
 
     /// ### Finish
-    ///
     /// With the `finish()` instruction a node can can post the result for a job it has finished,
     /// and be reimbursed for the work.
-    ///
     pub fn finish(ctx: Context<Finish>, ipfs_result: [u8; 32]) -> Result<()> {
         finish::handler(ctx, ipfs_result)
     }
 
     /// ### Quit
-    ///
     /// With the `quit()` instruction a node can quit a job that it has started.
-    ///
     pub fn quit(ctx: Context<Quit>) -> Result<()> {
         quit::handler(ctx)
     }
@@ -152,10 +124,8 @@ pub mod nosana_jobs {
     */
 
     /// ### Clean
-    ///
     /// The `clean()` instruction closes an [JobAccount](#job-account).
     /// The job has be finished and the job expiration time has to be exceeded.
-    ///
     pub fn clean(ctx: Context<Clean>) -> Result<()> {
         clean::handler(ctx)
     }
