aacb39b07d51d50968c19e911ce4accdd4bdab1f
Sjoerd Dijkstra <sjoerd@yogy.io>
2022-11-28 09:25:30 +0100

    Loosen the emission test to 2 seconds

--- tests/suites/2-nosana-staking-tests.ts
+++ tests/suites/2-nosana-staking-tests.ts
@@ -344,7 +344,7 @@ export default function suite() {
       expect(stake.duration.toNumber()).to.equal(duration, 'duration');
 
       const withDraw = balanceAfter - this.userBalanceBefore;
-      expect(withDraw).to.be.closeTo(expectedWithdraw, emission, 'withdraw'); // we allow 0 second error
+      expect(withDraw).to.be.closeTo(expectedWithdraw, 2 * emission, 'withdraw'); // we allow 2 second error
 
       this.balances.user += withDraw;
       this.balances.vaultStaking -= withDraw;
