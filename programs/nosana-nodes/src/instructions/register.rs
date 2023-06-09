7846077cce2d0d0fada6295565b49f99e4af58de
Sjoerd Dijkstra <sjoerd@yogy.io>
2023-06-09 14:39:24 +0200

    Remove redundant requires

--- programs/nosana-nodes/src/instructions/register.rs
+++ programs/nosana-nodes/src/instructions/register.rs
@@ -24,22 +24,6 @@ impl<'info> Register<'info> {
         endpoint: String,
         version: String,
     ) -> Result<()> {
-        require_neq!(
-            ArchitectureType::from(architecture_type) as u8,
-            ArchitectureType::Unknown as u8,
-            NosanaNodesError::ArchitectureUnknown
-        );
-        require_neq!(
-            CountryCode::from(country_code) as u8,
-            CountryCode::Unknown as u8,
-            NosanaNodesError::CountryCodeUnknown
-        );
-        require_gt!(cpu, 0, NosanaNodesError::CpuInvalid);
-        require_gt!(gpu, 0, NosanaNodesError::GpuInvalid);
-        require_gt!(memory, 0, NosanaNodesError::MemoryInvalid);
-        require_gt!(iops, 0, NosanaNodesError::IopsInvalid);
-        require_gt!(storage, 0, NosanaNodesError::StorageInvalid);
-
         self.node.register(self.authority.key())?;
         self.node.update(
             architecture_type,
